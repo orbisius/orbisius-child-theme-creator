a5d3c597feda9b59b8e40952f061611388de71b3
slavi <slavi@orbisius.com>
2017-04-28 18:03:42 +0300

    calling the right callback render_ui

--- addons/cloud_lib/modules/cloud.php
+++ addons/cloud_lib/modules/cloud.php
@@ -3,5 +3,5 @@
 $obj = new orbisius_ctc_cloud_lib();
 
 add_action( 'admin_init', array( $obj, 'init' ) );
-add_action( 'orbisius_child_theme_creator_editors_ext_action_left_start', [ $obj, 'render_tabs' ] );
+add_action( 'orbisius_child_theme_creator_editors_ext_action_left_start', [ $obj, 'render_ui' ] );
 
