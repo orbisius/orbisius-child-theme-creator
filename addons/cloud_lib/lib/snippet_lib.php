4eebe9c1ceb8c1ebe15d08e926a77e90abbd3844
slavi <slavi@orbisius.com>
2017-04-28 18:20:47 +0300

    added a method to render tab content (the method is based on the tab id name

--- addons/cloud_lib/lib/snippet_lib.php
+++ addons/cloud_lib/lib/snippet_lib.php
@@ -175,6 +175,65 @@ class orbisius_ctc_cloud_lib {
             </h2>
         <?php
     }
+    
+    public function render_tab_content( $tab_id = '' ) {
+        $tab_id = empty($tab_id) ? $this->get_current_tab_id() : $tab_id;
+        
+        $method_name = 'render_tab_content_' . $tab_id;
+        
+        if (method_exists( $this, $method_name)) {
+            $this->$method_name();
+        } else {
+            echo "Invalid tab method.";
+        }
+    }
+    
+    public function render_tab_content_orb_ctc_ext_cloud_lib_search() {
+        ?>
+        <!-- Search Snippets -->
+        <input class="selector" id="search_text"></input>
+        <input class="button button-primary" type="button" id="snippet_search_btn" value="Search" />
+        
+        <br />
+        <div class="found_snippet">
+            <strong>Snippet:</strong>
+            <textarea class="widefat" id="found_snippet_text"></textarea>
+            <strong>Title:</strong>
+            <input class="widefat" type="text" id="found_snippet_title"></textarea>
+        </div>
+        <!-- /Search Snippets -->
+        <?php
+    }
+    
+    
+    public function render_tab_content_orb_ctc_ext_cloud_lib_add() {
+        ?>
+         <!--New Snippet--> 
+        
+        <div class="new_snippet_wrapper">
+            <?php if ( ! orbisius_child_theme_creator_is_pro_installed() ) : ?>
+                    <span>Please, log in to add a snippet</span>
+                    <a class="pro_Add_On" href="//orbisius.com/products/wordpress-plugins/orbisius-child-theme-creator-pro/?utm_source=<?php echo $slug_area; ?>&utm_medium=action_screen&utm_campaign=product" target="_blank" title="[new window]">Pro Addon</a>
+            <?php endif; ?>
+            <?php if ( ! orbisius_child_theme_creator_is_pro_installed() ) : ?>
+                    <textarea class="widefat" id="add_snippet_text"></textarea>
+                    <br />
+                    <strong>Title</strong>
+                    <input type="text" id="add_snippet_title">
+                    <input class="button" type="button" id="snippet_save_btn" value="Save">
+            <?php endif; ?>
+        </div>
+        
+        <!-- Confirm dialog -->
+        <div id="snippet_confirm_dialog" title="">
+            <p>Are you sure you want to save a Snippet without any content?</p>
+        </div>
+        <!-- /Confirm dialog -->
+        <!-- /New Snippet -->
+
+        <?php
+    }
+    
     public function render_ui() {
         ?>
         <!-- Snippet Library Wrapper -->
@@ -182,44 +241,7 @@ class orbisius_ctc_cloud_lib {
                     <h3>Orbisius Cloud Library</h3>
 
                     <?php $this->render_tabs(); ?>
-                    
-                    <!-- New Snippet -->
-                    <input class="button" type="button" id="new_snippet_btn" value="Add a New Snippet">
-                    <br />
-                    <br />
-                    <div class="new_snippet_wrapper">
-                            <?php if ( ! orbisius_child_theme_creator_is_pro_installed() ) : ?>
-                                    <span>Please, log in to add a snippet</span>
-                                    <a class="pro_Add_On" href="//orbisius.com/products/wordpress-plugins/orbisius-child-theme-creator-pro/?utm_source=<?php echo $slug_area; ?>&utm_medium=action_screen&utm_campaign=product" target="_blank" title="[new window]">Pro Addon</a>
-                            <?php endif; ?>
-                            <?php if ( ! orbisius_child_theme_creator_is_pro_installed() ) : ?>
-                                    <textarea class="widefat" id="add_snippet_text"></textarea>
-                                    <br />
-                                    <strong>Title</strong>
-                                    <input type="text" id="add_snippet_title">
-                                    <input class="button" type="button" id="snippet_save_btn" value="Save">
-                            <?php endif; ?>
-                    </div>
-                    <!-- Confirm dialog -->
-                    <div id="snippet_confirm_dialog" title="">
-                            <p>Are you sure you want to save a Snippet without any content?</p>
-                    </div>
-                    <!-- /Confirm dialog -->
-                    <!-- /New Snippet -->
-
-                    <br />
-                    <!-- Search Snippets -->
-                    <input class="selector" id="search_text"></input>
-                    <input class="button button-primary" type="button" id="snippet_search_btn" value="Search">
-                    <br />
-                    <br />
-                    <div class="found_snippet">
-                            <strong>Snippet:</strong>
-                            <textarea class="widefat" id="found_snippet_text"></textarea>
-                            <strong>Title:</strong>
-                            <input class="widefat" type="text" id="found_snippet_title"></textarea>
-                    </div>
-                    <!-- /Search Snippets -->
+                    <?php $this->render_tab_content(); ?>
             </div>
             <!-- /Snippet Library Wrapper -->
         <?php
