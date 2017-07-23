ac628c69db1e1e5202c8bfc9dcbd23ab5d6e7564
slavi <slavi@orbisius.com>
2017-07-23 15:12:08 +0300

    fixed some methods and added get_user_id

--- lib/user.php
+++ lib/user.php
@@ -1,7 +1,7 @@
 <?php
 
 class orbisius_child_theme_creator_user extends orbisius_child_theme_creator_singleton {
-    private $api_meta_key = 'octc_api_key';
+    private $api_meta_key = '_orb_ctc_api_key';
     
     /**
      * 
@@ -9,7 +9,7 @@ class orbisius_child_theme_creator_user extends orbisius_child_theme_creator_sin
      * @return str
      */
     public function api_key($key = '') {
-        $user_id = wp_get_current_user_id();
+        $user_id = $this->get_user_id();
         
         if (!empty($key)) {
             $api_key = update_user_meta($user_id, $this->api_meta_key, $key);
@@ -18,5 +18,15 @@ class orbisius_child_theme_creator_user extends orbisius_child_theme_creator_sin
         $api_key = get_user_meta($user_id, $this->api_meta_key, true);
 
         return $api_key;
+    }
+    
+    /**
+     * 
+     * @param str/opt $key
+     * @return str
+     */
+    public function get_user_id() {
+        $user_id = get_current_user_id();
+        return $user_id;
     }
 }
