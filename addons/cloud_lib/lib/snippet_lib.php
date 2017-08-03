972a998733407922ba289aecddef1aa47c8fd078
slavi <slavi@orbisius.com>
2017-08-03 12:44:25 +0300

    added text_75p so the title of the snippet is large enough

--- addons/cloud_lib/lib/snippet_lib.php
+++ addons/cloud_lib/lib/snippet_lib.php
@@ -418,10 +418,10 @@ class orbisius_ctc_cloud_lib {
         <div id="orb_ctc_ext_cloud_lib_add" class="tabcontent orb_ctc_ext_cloud_lib_add">
             <div id="new_snippet_wrapper" class="new_snippet_wrapper">
                 <form method="POST" id="orb_ctc_ext_cloud_lib_add_new_snippet_form" class="orb_ctc_ext_cloud_lib_add_new_snippet_form">
-                    <textarea id="add_snippet_text" class="widefat add_snippet_text" required=""></textarea>
+                    <textarea id="add_snippet_text" class="widefat add_snippet_text" required="" placeholder="Snippet content"></textarea>
                     <br />
                     <strong>Title</strong>
-                    <input type="text" id="add_snippet_title" value="" />
+                    <input type="text" id="add_snippet_title" class="add_snippet_title text_75p" value="" />
                     <br />
                     <input type="submit" id="snippet_save_btn" class="button button-primary snippet_save_btn" value="Save" /> 
                 </form>
