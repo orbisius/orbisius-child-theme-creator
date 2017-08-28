39e48348936c0343524059d0dd46cc8a95c0511c
slavi <slavi@orbisius.com>
2017-08-28 15:06:08 +0300

    added feedback on copy text and the selection is cleared.

--- addons/clipboard/assets/main.js
+++ addons/clipboard/assets/main.js
@@ -6,12 +6,19 @@ jQuery(document).ready(function($) {
         var clipboard = new Clipboard('.orb_ctc_copy_btn');
 
         clipboard.on('success', function(e) {
-            console.log(e);
+            e.clearSelection();
+            var btn = $(e.trigger);
+            var old_label = btn.text();
+            
+            btn.text('Copied!');
+
+            setTimeout(function () {
+                btn.text(old_label);
+            }, 1500);
         });
 
         clipboard.on('error', function(e) {
             alert('copy failed');
-            console.log(e);
         });
     } catch (e) {
         console && console.log("orbisius child theme creator addon: Clipboard.js wasn't loaded");
