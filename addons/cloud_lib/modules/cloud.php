fe9c51a532f4e226e4b4b1994611bd2d62464bed
slavi <slavi@orbisius.com>
2017-09-01 05:24:31 +0300

    aa

--- addons/cloud_lib/modules/cloud.php
+++ addons/cloud_lib/modules/cloud.php
@@ -3,5 +3,5 @@
 $obj = new orbisius_ctc_cloud_lib();
     
 add_action( 'admin_init', array( $obj, 'admin_init' ) );
-add_action( 'orbisius_child_theme_creator_editors_ext_action_left_start', [ $obj, 'render_ui' ] );
+add_action( 'orbisius_child_theme_creator_editors_ext_action_left_start', array( $obj, 'render_ui' ) );
 
