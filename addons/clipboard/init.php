8c49ca568a7d497338d95540fede32ee30d34a99
slavi <slavi@orbisius.com>
2017-09-11 22:14:51 +0300

    orb_ctc_addon_clipboard extends orbisius_child_theme_creator_singleton

--- addons/clipboard/init.php
+++ addons/clipboard/init.php
@@ -1,9 +1,9 @@
 <?php
 
-$c = new orb_ctc_addon_clipboard();
+$c = orb_ctc_addon_clipboard::get_instance();
 add_action('orbisius_child_theme_creator_admin_enqueue_scripts', array( $c, 'admin_enqueue_scripts' ) );
 
-class orb_ctc_addon_clipboard {
+class orb_ctc_addon_clipboard extends orbisius_child_theme_creator_singleton {
     function admin_enqueue_scripts() {
         wp_register_script( 'orb_ctc_addon_clipboard', 
             apply_filters('orbisius_child_theme_creator_filter_asset_src', "addons/clipboard/share/clipboard-js/dist/clipboard.min.js"), 
