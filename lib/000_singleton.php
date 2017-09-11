537a3d908f7b85bfe58831314cad879d33fc7013
slavi <slavi@orbisius.com>
2017-09-11 22:20:23 +0300

    returned the check for php 4.3 version

--- lib/000_singleton.php
+++ lib/000_singleton.php
@@ -1,6 +1,6 @@
 <?php
 
-if (0&&version_compare(phpversion(), '5.3', '>=')) {
+if (version_compare(phpversion(), '5.3', '>=')) {
     $singleton_file = dirname(__FILE__) . '/000_singleton.inc';
 } else { // less than 5.2
     $singleton_file = dirname(__FILE__) . '/000_singleton_compat.inc';
